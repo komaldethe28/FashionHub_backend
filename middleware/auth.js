5e55c6756514548df0fcda85e0eb6c1c5196d13d
komal <dethekomalv@gmail.com>
2026-03-03 13:48:03 +0530

    Establishing the connection with the Cart logic

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+
+const authUser = async (req, res, next) => {
+
+    const { token } = req.headers;
+
+    if (!token) {
+        return res.json({ success: false, message: 'Unauthorized LOgin Required' })
+    }
+
+    try {
+        const tocken_decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.body.userId = tocken_decoded.id;
+        next();
+    } catch (error) {
+        return res.json({ success: false, message: error.message })
+    }
+}
+    export default authUser;
