754af13956d5c208dab86e88624a519682a528b5
komal <dethekomalv@gmail.com>
2026-03-03 12:34:50 +0530

    Product is visible on webpage

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,7 +20,7 @@ const loginUser = async (req, res) => {
             return res.json({ success: false, message: "User not exists" })
         }
         const isMatch = await bcrypt.compare(password, user.password)
-        if (!isMatch) {
+        if (isMatch) {
             const token = createToken(user._id)
             res.json({ success: true, token })
         }
